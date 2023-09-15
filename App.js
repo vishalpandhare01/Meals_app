bb5305934818262d79a5ec2f8d3bfd940f8d049a
vishal pandhare <vishalpandhare2016@gmail.com>
2023-09-15 18:36:07 +0530

    updatred code

--- App.js
+++ App.js
@@ -5,8 +5,32 @@ import { NavigationContainer } from "@react-navigation/native"; //install  npm i
 import { createNativeStackNavigator } from "@react-navigation/native-stack"; //installl
 import MealsOverviewScreen from "./screens/mealOverviewScreen";
 import MealDetialsscreen from "./screens/MealDetialsscreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Disclaimer from "./screens/DisclaimerScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigatior() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#cccccc" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen
+        name="categories"
+        component={CatogorysScreen}
+        options={{
+          title: "All Categories",
+        }}
+      />
+      <Drawer.Screen name="Disclaimer" component={Disclaimer} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -21,10 +45,11 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="mealCatgories"
-            component={CatogorysScreen}
+            name="Drawer"
+            component={DrawerNavigatior}
             options={{
-              title: "All Categories",
+              // title: "All Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen
@@ -35,13 +60,13 @@ export default function App() {
             //   return {title:catId};
             // }}
           />
-           <Stack.Screen
+          <Stack.Screen
             name="MealDetial"
             component={MealDetialsscreen}
             // options={{headerRight:()=>{
             //   return <Button title="Tap me" onPress={} />
             // }}}
-            />
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ screens/DisclaimerScreen.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Disclaimer(){
+    return(
+        <View> 
+            <Text>Disclaimer !!!</Text>
+            <Text>The reader must take the responsibility to review the listed ingredients before cooking to ensure none of the ingredients causes potential adverse effects to</Text>
+        </View>
+    )
+}
