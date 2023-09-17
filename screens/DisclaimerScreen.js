93da47c6a307c6e1c522cbe7f9152c195668b47f
vishal pandhare <vishalpandhare2016@gmail.com>
2023-09-17 08:16:08 +0530

    Disclaimer updated

--- screens/DisclaimerScreen.js
+++ screens/DisclaimerScreen.js
@@ -1,10 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Disclaimer(){
     return(
-        <View> 
-            <Text>Disclaimer !!!</Text>
+        <View style={styles.container}> 
+            <Text style={styles.heading}>Disclaimer !!!</Text>
             <Text>The reader must take the responsibility to review the listed ingredients before cooking to ensure none of the ingredients causes potential adverse effects to</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        padding:8,
+        margin:8
+    },
+    heading:{
+        fontSize:24,
+        textAlign:'center',
+    }
+
+
+})
